6686a741f9cee8003227080498b76b845b6bac34
dokan722 <peiter20000@gmail.com>
2025-06-12 17:32:54 +0200

    3423 (daily)

---
+++ C#/Problems/Problems/Problems/3423.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Problems
+{
+    public class _3423 :IProblem
+    {
+        public bool Test()
+        {
+            var nums = new int[] { 1, 2, 4 };
+
+
+            var expected = 3;
+
+            var result = MaxAdjacentDistance(nums);
+
+            return expected == result;
+        }
+
+
+        public int MaxAdjacentDistance(int[] nums)
+        {
+            var maxDiff = Math.Abs(nums[nums.Length - 1] - nums[0]);
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                var diff = Math.Abs(nums[i] - nums[i + 1]);
+                if (diff > maxDiff)
+                    maxDiff = diff;
+            }
+
+            return maxDiff;
+        }
+    }
+}
--- C#/Problems/Problems/Program.cs
+++ C#/Problems/Problems/Program.cs
@@ -2,7 +2,7 @@
 using Problems.Problems;
 
 
-var problem = new _5();
+var problem = new _3423();
 
 if (problem.Test())
 {
