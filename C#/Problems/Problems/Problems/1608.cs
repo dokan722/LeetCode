d87006c778b9701047667de28f298e5b8f4e5c42
dokan722 <peiter20000@gmail.com>
2026-07-22 20:12:12 +0200

    1608, 3151

---
+++ C#/Problems/Problems/Problems/1608.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Problems
+{
+    public class _1608 : IProblem
+    {
+        public bool Test()
+        {
+            var nums = new[] { 3, 5 };
+
+            var expected = 2;
+
+            var result = SpecialArray(nums);
+
+            Console.WriteLine(result);
+
+            return result == expected;
+        }
+
+        public int SpecialArray(int[] nums)
+        {
+            var counts = new int[1001];
+            foreach (var n in nums)
+                counts[n]++;
+            var cur = 0;
+            for (int i = 1000; i >= 0; --i)
+            {
+                cur += counts[i];
+                if (cur == i)
+                    return i;
+                if (cur > i)
+                    break;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ C#/Problems/Problems/Problems/3151.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Problems
+{
+    public class _3151 : IProblem
+    {
+        public bool Test()
+        {
+            var nums = new[] { 2, 1, 4 };
+
+            var expected = true;
+
+            var result = IsArraySpecial(nums);
+
+            return expected == result;
+        }
+
+        public bool IsArraySpecial(int[] nums)
+        {
+            var n = nums.Length;
+            for (int i = 0; i < n - 1; ++i)
+                if (nums[i] % 2 == nums[i + 1] % 2)
+                    return false;
+
+            return true;
+        }
+    }
+}
